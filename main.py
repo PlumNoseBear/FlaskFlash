c989881458d71f8bae64f16a067c05c53133703d
PlumNoseBear <46938139+PlumNoseBear@users.noreply.github.com>
2023-10-15 19:44:42 +0000

    v.0.7
    сохраняем и занимаемся чем-то другим

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-from flask import Flask, flash, redirect, render_template, request, url_for
-import time
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+import os
+from tqdm import tqdm
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
+app.secret_key = "super_secret_key"
+app.config['UPLOAD_PATH'] = 'static/files'
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        flash('Button was clicked!')
-        time.sleep(1)  # Задержка в 1 секунду
-        return redirect(url_for('index'))
+def upload_with_progress(file):
+    chunk_size = 4096
+    total_size = os.fstat(file.fileno()).st_size
+    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+    with open(os.path.join(app.config['UPLOAD_PATH'], file.filename), 'wb') as f:
+        while True:
+            data = file.stream.read(chunk_size)
+            if not data:
+                break
+            f.write(data)
+            progress_bar.update(len(data))
+            progress = int((progress_bar.n / progress_bar.total) * 100)
+            flash('Загрузка: {}%'.format(progress), 'progress')
+    progress_bar.close()
+
+    flash('Файл загружен', 'success')
+    return redirect(url_for('upload_form'))
+
+@app.route('/')
+def upload_form():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        filename = file.filename
+        upload_with_progress(file)
+        return redirect(url_for('upload_form'))
+    else:
+        flash('Не выбран файл', 'error')
+        return redirect(request.url)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
